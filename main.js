64d061874c561a825bcde73d80f5af862f7e2ada
Bleb1k <viktor.masnikov17283940@gmail.com>
2024-12-13 10:53:51 +0900

    Added `for capture`

--- main.js
+++ main.js
@@ -1,42 +1,64 @@
+import { splitParameters } from "./lib/lib.js";
+
 const args = Deno.args;
 
 if (!args.length) {
-    Deno.exit((console.log("Supply file name"), 1));
+  Deno.exit((console.log("Supply file name"), 1));
 }
 
-const file = Deno.readTextFileSync(Deno.realPathSync(Deno.args[0]));
+const filePath = Deno.realPathSync(Deno.args[0]);
+let file = Deno.readTextFileSync(filePath)
 
 const includes = [];
 let result = "";
-
-for (const match of file.matchAll(
-    /(?<range>\d+\.\.(?:=(?=\d))?\d*)|(?<word>\w+)|(?<paren>[<>(){}[\]])|(?<comment>\/\/.*$|\/\*[^]*?\*\/)|(?<punctuation>([^])\6*)/gm
-)) {
+let needsRerun = true
+let foo = 2
+while (needsRerun) {
+  needsRerun = false
+  for (const match of file.matchAll(
+    /(?<forCapture>for *\( *(?<FCIterable>[^)]*?) *\) *\| *(?<FCVariable>[^|]*?) *\|)|(?<range>\d+\.\.(?:=(?=\d))?\d*)|(?<word>\w+)|(?<paren>[<>(){}[\]])|(?<comment>\/\/.*$|\/\*[^]*?\*\/)|(?<punc>(?<puncInner>[^])\k<puncInner>*)/gm
+  )) {
     const buf = Object.entries(match.groups).filter(
-        ([_, v]) => v !== undefined,
+      ([_, v]) => v !== undefined,
     )[0];
 
     switch (buf[0]) {
-        case "range": {
-            const including = buf[1] !== (buf[1] = buf[1].replace("=", ""));
-            let [min, max] = buf[1].split("..");
-            if (max === "" || max - min > 1000) {
-                if (!includes.includes("range")) includes.push("range");
-                buf[1] = `range(${max === "" ? min : [min, max]})`;
-                break;
-            }
-            [min, max] = [min, max].map(Number);
-            if (including) max += 1;
-            buf[1] = `new Array(${max - min}).fill(${min}).map((n, i) => n + i)`;
-            break;
+      case "forCapture":
+        needsRerun = true
+        if (!includes.includes("FCIter")) includes.push("FCIter")
+        const { FCIterable, FCVariable } = match.groups
+        const [iterables, variables] = [splitParameters(FCIterable), splitParameters(FCVariable)]
+        if (iterables.length < variables.length) {
+          // TODO: getPosition -> `${line}:${index}`
+          throw new Error(`Can't capture ${variables.length} parameters, ${iterables.length} supplied.\n    at file://${filePath} index:${match.index}\n        ${match[0]}`);
         }
-        default:
-            break;
+        while (iterables.length > variables.length) variables.push("_")
+        buf[1] = `for (const [${variables}] of FCIter(${iterables}))`
+        break;
+      case "range": {
+        const including = buf[1] !== (buf[1] = buf[1].replace("=", ""));
+        let [min, max] = buf[1].split("..");
+        if (max === "" || max - min > 1000) {
+          if (!includes.includes("range")) includes.push("range");
+          buf[1] = `range(${max === "" ? min : [min, max]})`;
+          break;
+        }
+        [min, max] = [min, max].map(Number);
+        if (including) max += 1;
+        buf[1] = `new Array(${max - min}).fill(${min}).map((n, i) => n + i)`;
+        break;
+      }
+      default:
+        break;
     }
 
     result += buf[1];
+  }
+  if (needsRerun) {
+    file = result
+    result = ""
+  }
 }
 
 result = `import { ${includes.toString()} } from "./std.js";\n${result}`;
-console.log("Done compiling!!\n\n", result);
 Deno.writeTextFileSync(Deno.realPathSync(Deno.args[0]) + ".js", result);
--- std.js
+++ std.js
@@ -1,19 +1,32 @@
 export function* range(from, to) {
-    if (to ?? 0) {
-        while (from < to) yield from++;
-    } else {
-        while (true) yield from++;
-    }
+  if (to ?? 0) {
+    while (from < to) yield from++;
+  } else {
+    while (true) yield from++;
+  }
 }
 
+export function* FCIter(...iterables) {
+  const arrays = iterables.filter(o => o instanceof Array)
+  const next = (obj, i) => obj instanceof Array ? obj[i] : obj.next().value;
+  let i = 0
+  if (arrays.length === 0) {
+    while (true) yield iterables.map(v => next(v))
+  };
+  const max = arrays.reduce((a, b) => Math.max(a, b.length), 0)
+  if (max > 0) {
+    while (i < max) { yield iterables.map(v => next(v, i)); ++i }
+  }
+  return;
+}
 class Foo {
-    #a;
-    // either
-    // constructor(str = "default") {
-    //     this.#a = str;
-    // }
-    // or
-    // constructor({ str } = { str: "default" }) {
-    //     this.#a = str;
-    // }
+  #a;
+  // either
+  // constructor(str = "default") {
+  //     this.#a = str;
+  // }
+  // or
+  // constructor({ str } = { str: "default" }) {
+  //     this.#a = str;
+  // }
 }
